203e04d7b0e46d9466903fe62673223208b5204f
abrorivich <abroriv1ch0414@gmail.com>
2024-10-26 22:39:05 +0500

    create  create-course.dto and update-course.dt

--- src/course/dto/create-course.dto.ts
+++ src/course/dto/create-course.dto.ts
@@ -1,1 +1,33 @@
-export class CreateCourseDto {}
+import { IsNotEmpty, IsNumber, IsString, Max, MaxLength, Min, MinLength } from "class-validator";
+
+export class CreateCourseDto {
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(32)
+    @MinLength(4)
+    name: string
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(10)
+    @MaxLength(128)
+    description: string
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Min(500000)
+    @Max(2000000)
+    price: number
+
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(32)
+    @MinLength(4)
+    category: string
+
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(64)
+    @MinLength(4)
+    level: string
+}
--- src/course/dto/update-course.dto.ts
+++ src/course/dto/update-course.dto.ts
@@ -1,4 +1,35 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCourseDto } from './create-course.dto';
+import { IsNumber, IsOptional, IsString, Max, MaxLength, Min, MinLength } from 'class-validator';
 
-export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {
+    @IsOptional()
+    @IsString()
+    @MaxLength(32)
+    @MinLength(4)
+    name: string
+    
+    @IsOptional()
+    @IsString()
+    @MinLength(10)
+    @MaxLength(128)
+    description: string
+    
+    @IsOptional()
+    @IsNumber()
+    @Min(500000)
+    @Max(2000000)
+    price: number
+    
+    @IsOptional()
+    @IsString()
+    @MaxLength(32)
+    @MinLength(4)
+    category: string
+    
+    @IsOptional()
+    @IsString()
+    @MaxLength(64)
+    @MinLength(4)
+    level: string
+}
