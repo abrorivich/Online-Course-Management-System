4edb6142feb383c6e1fb2c2685e050bb947dd725
abrorivich <abroriv1ch0414@gmail.com>
2024-10-26 22:47:21 +0500

    create create-lesson.dto and update-lesson.dto

--- src/lessons/dto/create-lesson.dto.ts
+++ src/lessons/dto/create-lesson.dto.ts
@@ -1,1 +1,19 @@
-export class CreateLessonDto {}
+import { IsNotEmpty, IsNumber, IsString, MaxLength, MinLength } from "class-validator"
+
+export class CreateLessonDto {
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(32)
+    name: string
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(32)
+    description: string
+
+    @IsNotEmpty()
+    @IsNumber()
+    modulesId: number
+}
--- src/lessons/dto/update-lesson.dto.ts
+++ src/lessons/dto/update-lesson.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateLessonDto } from './create-lesson.dto';
+import { IsNumber, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
-export class UpdateLessonDto extends PartialType(CreateLessonDto) {}
+export class UpdateLessonDto extends PartialType(CreateLessonDto) {
+    @IsOptional()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(32)
+    name: string
+
+    @IsOptional()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(32)
+    description: string
+
+    @IsOptional()
+    @IsNumber()
+    modulesId: number
+}
