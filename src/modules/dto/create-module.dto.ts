c63f957ad1a290186e015555de96be1655cfdcea
abrorivich <abroriv1ch0414@gmail.com>
2024-10-26 22:43:57 +0500

    create  create-module.dto and update-module.dt

--- src/modules/dto/create-module.dto.ts
+++ src/modules/dto/create-module.dto.ts
@@ -1,1 +1,19 @@
-export class CreateModuleDto {}
+import { IsNotEmpty, IsNumber, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateModuleDto {
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(32)
+    name: string
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(32)
+    description: string
+
+    @IsNotEmpty()
+    @IsNumber()
+    courseId: number
+}
--- src/modules/dto/update-module.dto.ts
+++ src/modules/dto/update-module.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateModuleDto } from './create-module.dto';
+import { IsNumber, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
-export class UpdateModuleDto extends PartialType(CreateModuleDto) {}
+export class UpdateModuleDto extends PartialType(CreateModuleDto) {
+    @IsOptional()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(32)
+    name: string
+
+    @IsOptional()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(32)
+    description: string
+
+    @IsOptional()
+    @IsNumber()
+    courseId: number
+}
