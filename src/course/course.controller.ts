39510d5b121837b2b7097b5c42a1a59386dcf72a
abrorivich <abroriv1ch0414@gmail.com>
2024-10-28 02:23:49 +0500

    course.service and course.controller

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,34 +1,40 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { Course } from './entities/course.entity';
 
 @Controller('course')
 export class CourseController {
-  constructor(private readonly courseService: CourseService) {}
+  constructor(private readonly courseService: CourseService) { }
 
-  @Post()
-  create(@Body() createCourseDto: CreateCourseDto) {
+  @UseGuards(AuthGuard, RolesGuard)
+  @Post("create")
+  async create(@Body() createCourseDto: CreateCourseDto): Promise<Course> {
     return this.courseService.create(createCourseDto);
   }
 
-  @Get()
-  findAll() {
+  @Get("getAll")
+  findAll(): Promise<Course[]> {
     return this.courseService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get('getById/:id')
+  findOne(@Param('id') id: number): Promise<Course> {
     return this.courseService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
+  @UseGuards(AuthGuard, RolesGuard)
+  @Patch('update/:id')
+  update(@Param('id') id: number, @Body() updateCourseDto: UpdateCourseDto): Promise<string> {
     return this.courseService.update(+id, updateCourseDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @UseGuards(AuthGuard, RolesGuard)
+  @Delete('delete/:id')
+  remove(@Param('id') id: number): Promise<string> {
     return this.courseService.remove(+id);
   }
 }
--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { Course } from './entities/course.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CourseService {
-  create(createCourseDto: CreateCourseDto) {
-    return 'This action adds a new course';
+  constructor(
+    @InjectRepository(Course)
+    private courseRepository: Repository<Course>,
+  ) { }
+
+  async create(createCourseDto: CreateCourseDto): Promise<Course> {
+    const course = await this.courseRepository.create(createCourseDto);
+    return this.courseRepository.save(course);
   }
 
-  findAll() {
-    return `This action returns all course`;
+  async findAll(): Promise<Course[]> {
+    const course = await this.courseRepository.find()
+    return course;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} course`;
+  async findOne(id: number): Promise<Course> {
+    let course = await this.courseRepository.findOneBy({ id })
+    if (!course)
+      throw new HttpException('Course not found', HttpStatus.NOT_FOUND);
+    return course;
   }
 
-  update(id: number, updateCourseDto: UpdateCourseDto) {
-    return `This action updates a #${id} course`;
+  async update(id: number, updateCourseDto: UpdateCourseDto): Promise<string> {
+    let coruse = await this.courseRepository.findOneBy({ id })
+    if (!coruse) {
+      throw new HttpException('Course not found', HttpStatus.NOT_FOUND);
+    }
+    await this.courseRepository.update({ id }, { ...updateCourseDto });
+    return `Updated course 👌🏻`
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} course`;
+  async remove(id: number): Promise<string> {
+    let course = await this.courseRepository.findOneBy({ id })
+    if (!course) {
+      throw new HttpException('Course not found', HttpStatus.NOT_FOUND);
+    }
+    await this.courseRepository.delete(id);
+    return "Deleted 🛒"
   }
 }
